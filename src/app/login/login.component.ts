600a8688c5b1a22837c38e140dd81019e50fca3c
Gilberto <gilbertodominguezfigueroa@gmail.com>
2021-10-31 13:51:21 -0600

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BlogrestService } from '../blogrest.service';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  //TypeScript
+  titulo= "Inicio de sesión";
+  user="";
+  pass="";
 
-  constructor() { }
+  login() {
+    this.blogrest.login(this.user, this.pass).subscribe(resp => {
+      console.log(resp);
+    }, error => {
+      console.log(error);
+    })
+    //this.rt.navigate(['/inicio']);
+  }
+
+  constructor(private rt: Router, private blogrest: BlogrestService) { }
 
   ngOnInit(): void {
   }
